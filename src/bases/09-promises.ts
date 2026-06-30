0013b389fbdc6a1f04ce62faf60dc0c2430e10f0
JAriza <arizaj@uninet.com.mx>
2026-06-30 12:10:53 -0600

    fix(promises): corregir manejo de promesas al rechazar en lugar de resolver

--- src/bases/09-promises.ts
+++ src/bases/09-promises.ts
@@ -1,13 +1,19 @@
-const myPromise = new Promise( (resolve) => {
+const myPromise = new Promise<number>((resolve, reject) => {
 
     setTimeout(() => {
-        resolve(100);
+        // resolve(100);
+        reject("Se perdió");
     }, 2000);
 
 })
 
-myPromise.then(
-    (myMoney) => {
+myPromise
+    .then((myMoney) => {
         console.log(`Tengo mi dinero ${myMoney}`);
-    }
-)
+    })
+    .catch(reason => {
+        console.warn(reason);
+    })
+    .finally(() => {
+        console.log("A seguir con la vida");
+    });
